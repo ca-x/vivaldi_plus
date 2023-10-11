b0b60ef48df381202b2b0adde7ccc21d0df4a8f4
czyt <czyt@w.cn>
2023-10-11 15:04:48 +0800

    update version

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 #define RELEASE_VER_MAIN 1
 #define RELEASE_VER_SUB 5
-#define RELEASE_VER_FIX 6
-#define RELEASE_VER_PATCH 1
+#define RELEASE_VER_FIX 7
+#define RELEASE_VER_PATCH 0
 
 #define TOSTRING2(arg) #arg
 #define TOSTRING(arg) TOSTRING2(arg)
